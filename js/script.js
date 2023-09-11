be14f10274223e98144578a3aeadc9e73daa013f
Muhammad Fathur Rachman <muhammad.fathur.rachman01@gmail.com>
2023-09-11 16:37:22 +0700

    Change the display of the number formula in index.html and slightly improve hitungKeliling function

--- js/script.js
+++ js/script.js
@@ -4,17 +4,23 @@
 
 const btnSubmit = document.getElementsByClassName("btn-submit");
 const inputNumber = document.getElementById("number").value;
+const sisi1 = document.getElementsByClassName("sisi1");
+const sisi2 = document.getElementsByClassName("sisi2");
 
 btnSubmit[0].addEventListener("click", () => {
-  hitungLuas();
+  // hitungLuas();
+  hitungKeliling();
 });
 
 function hitungLuas() {
+  sisi1[0].innerHTML = inputNumber;
+  sisi2[0].innerHTML = inputNumber;
   const result = inputNumber * inputNumber;
   document.querySelector("section#luas p:last-of-type span").innerHTML = result;
 }
 
 function hitungKeliling() {
+  sisi1[0].innerHTML = inputNumber;
   const result = 4 * inputNumber;
   document.querySelector("section#keliling p:last-of-type span").innerHTML =
     result;
