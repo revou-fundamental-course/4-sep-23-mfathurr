77126cd3065674dd57eb08d94a05ad27e9db38f5
Muhammad Fathur Rachman <muhammad.fathur.rachman01@gmail.com>
2023-09-12 19:06:39 +0700

    Bug fixing input, button submit & button reset function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // 1. Buat section show/hide dengan kelas active/disabled
-const navHitungLuas = document.querySelector("nav a:nth-child(1)");
+const navHitungLuas = document.querySelector("nav button:nth-child(1)");
 console.log(navHitungLuas);
-const navHitungKeliling = document.querySelector("nav a:nth-child(2)");
+const navHitungKeliling = document.querySelector("nav button:nth-child(2)");
 console.log(navHitungKeliling);
 const sectionLuas = document.querySelector("section#luas");
 const sectionKeliling = document.querySelector("section#keliling");
@@ -18,32 +18,60 @@ navHitungKeliling.addEventListener("click", () => {
 
 // 2. Cek status aktif section & jalankan fungsi addEventListener sesuai status aktif
 const btnSubmit = document.getElementsByClassName("btn-submit");
-const inputNumber = document.getElementById("number").value;
 const sisi1 = document.getElementsByClassName("sisi1");
 const sisi2 = document.getElementsByClassName("sisi2");
 
 btnSubmit[0].addEventListener("click", () => {
-  if (document.querySelector("section#luas > .btn-action > .btn-submit")) {
-    hitungLuas();
-  } else if (
-    document.querySelector("section#keliling > .btn-action > .btn-submit")
-  ) {
-    hitungKeliling();
-  }
+  sectionLuas.classList.contains("active")
+    ? hitungLuas()
+    : "Terjadi kesalahan hitung luas";
+});
+
+btnSubmit[1].addEventListener("click", () => {
+  sectionKeliling.classList.contains("active")
+    ? hitungKeliling()
+    : "Terjadi kesalahan hitung keliling";
 });
 
 function hitungLuas() {
-  sisi1[0].innerHTML = inputNumber;
-  sisi2[0].innerHTML = inputNumber;
-  const result = inputNumber * inputNumber;
-  console.log(result);
-  document.querySelector("section#luas p:last-of-type span").innerHTML = result;
+  const inputNumber = document.getElementById("numberLuas").value;
+  sisi1[0].innerText = inputNumber;
+  sisi2[0].innerText = inputNumber;
+  document.querySelector("section#luas p:last-of-type span").innerText =
+    inputNumber * inputNumber;
 }
 
 function hitungKeliling() {
+  const inputNumber = document.getElementById("numberKeliling").value;
   sisi1[0].innerHTML = inputNumber;
-  const result = 4 * inputNumber;
-  console.log(result);
   document.querySelector("section#keliling p:last-of-type span").innerHTML =
-    result;
+    4 * inputNumber;
+}
+
+// 4. Reset button
+const btnReset = document.getElementsByClassName("btn-reset");
+
+btnReset[0].addEventListener("click", () => {
+  sectionLuas.classList.contains("active")
+    ? resetLuas()
+    : "Terjadi kesalahan reset luas";
+});
+
+btnReset[1].addEventListener("click", () => {
+  sectionKeliling.classList.contains("active")
+    ? resetKeliling()
+    : "Terjadi kesalahan reset keliling";
+});
+
+function resetLuas() {
+  document.getElementById("numberLuas").value = "";
+  sisi1[0].innerHTML = 0;
+  sisi2[0].innerHTML = 0;
+  document.querySelector("section#luas p:last-of-type span").innerHTML = 0;
+}
+
+function resetKeliling() {
+  document.getElementById("numberKeliling").value = "";
+  sisi1[0].innerHTML = 0;
+  document.querySelector("section#keliling p:last-of-type span").innerHTML = 0;
 }
